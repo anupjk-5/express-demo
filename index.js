d82daff4961ef97ba726a40b2c3747a831118e39
anupjk_5 <anupjyoti.kalita@borgos.in>
2021-10-25 17:28:46 +0530

    node vs express

--- index.js
+++ index.js
@@ -1,78 +1,22 @@
-const express = require("express");
-const { executeQuery, getId } = require("./helpers/utils");
-const app = express();
+const http = require('http');
 
-// middlewares
-app.use(express.json({ extended: true }));
+const server = http.createServer((req, res) => {
+    const url = req.url;
 
-// routes
-app.get('/books', async (req, res) => {
-    const query = 'SELECT * FROM books';
-    try {
-        const result = await executeQuery(query, []);
-        res.json({ result });
-    } catch (error) {
-        res.json({ error });
+    if (url === '/') {
+        res.write('<html>');
+        req.write('<head><title>Node Server</title></head>');
+        res.write('<body><h2>This is a h2 tag</h2></body>');
+        res.write('</html>');
+        return res.end();
+    } 
+    if (url === '/about') {
+        res.write('<html>');
+        req.write('<head><title>Node Server</title></head>');
+        res.write('<body><h1>This is a h1 tag</h1></body>');
+        res.write('</html>');
+        return res.end();
     }
 });
 
-app.post('/books', async (req, res) => {
-    const { name, author, publishedOn } = req.body;
-    const id = getId();
-    const newRow = { id, name, author, publishedOn };
-
-    const query = 'INSERT INTO books(id, name, author, published_on) VALUES(?, ?, ?, ?)';
-    try {
-        await executeQuery(query, [id, name, author, publishedOn]);
-        res.json(newRow);
-    } catch (error) {
-        res.json({ error });
-    }
-});
-
-app.patch('/books/:id', async (req, res) => {
-    const { id } = req.params;
-    let keyValuePairs = '';
-    const params = [];
-    let index = 0;
-
-    Object.keys(req.body).forEach((key) => {
-        keyValuePairs += `${key}=?`;
-        params.push(req.body[key]);
-
-        if (index < Object.keys(req.body).length - 1) {
-            keyValuePairs += ',';
-        }
-
-        index += 1;
-    });
-
-    const query1 = `UPDATE books SET ${keyValuePairs} WHERE id=?;`;
-    const query2 =  "SELECT * FROM books WHERE id=?";
-    try {
-        await executeQuery(query1, [...params, id]);
-        const result2 = await executeQuery(query2, [id]);
-        res.json({ result: result2 });
-    } catch (error) {
-        res.json({ error });
-    }
-});
-
-app.delete('/books/:id', async (req, res) => {
-    const { id } = req.params;
-
-    const query = "DELETE FROM books WHERE id=?";
-    try {
-        await executeQuery(query, [id]);
-        res.json({ msg: "The entry is removed" });
-    } catch (error) {
-        res.json({ error });
-    }
-});
-
-// server starting
-app.listen(4000, () => {
-    console.log('Server is running at port 4000');
-});
-
-
+server.listen(4000, () => console.log('Server is running at 4000'));
